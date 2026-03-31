eb74f97d0f288f3f60ce9b9d51c333391a8d92d5
Hugo Orielso <orielso.lozano15@gmail.com>
2026-03-31 04:10:30 -0500

    eliminando routesimplificando route

--- src/components/common/LogoutButton.tsx
+++ src/components/common/LogoutButton.tsx
@@ -2,32 +2,11 @@
 
 import { signOut } from "next-auth/react";
 import { LogOut } from "lucide-react";
-import { toast } from "sonner";
-import { useRouter } from "next/navigation";
 
 export default function LogoutButton() {
-  const router = useRouter();
-
-  const handleLogout = async () => {
-    const toastId = toast.loading("Cerrando sesión...");
-
-    try {
-      const res = await signOut({
-        redirect: false,
-        redirectTo: "/login",
-      });
-
-      toast.success("Sesión cerrada correctamente.", { id: toastId });
-      router.replace(res.url || "/login");
-    } catch (error) {
-      console.error(error);
-      toast.error("Error al cerrar sesión. Intenta de nuevo.", { id: toastId });
-    }
-  };
-
   return (
     <button
-      onClick={handleLogout}
+      onClick={() => signOut({ callbackUrl: "/login" })}
       className="flex items-center gap-2 text-sm text-red-500 hover:text-red-700 transition font-medium"
     >
       <LogOut className="w-4 h-4" />
