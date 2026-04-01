cbc96b5cd0168731a6290a6d807759128233ee8c
Hugo Orielso <orielso.lozano15@gmail.com>
2026-04-01 05:04:40 -0500

    probando

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "standalone",
+  experimental: {
+    outputFileTracingIncludes: {
+      "/**": ["./node_modules/next-auth/**/*"],
+    },
+  } as NextConfig["experimental"],
   async headers() {
     return [
       {
