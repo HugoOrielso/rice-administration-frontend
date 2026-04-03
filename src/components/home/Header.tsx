d9532047528f8a7bcd2475c7f43ac8836b036da1
Hugo Orielso <orielso.lozano15@gmail.com>
2026-04-03 03:06:38 -0500

    pequeño cambio en header

--- src/components/home/Header.tsx
+++ src/components/home/Header.tsx
@@ -10,9 +10,9 @@ const navItems: { label: string; href: string }[] = [
 export function Header() {
   return (
     <header className="sticky top-0 z-50 border-b border-(--color-brand-soft) bg-white/95 backdrop-blur">
-      <div className="mx-auto flex max-w-7xl items-center justify-between px-6 py-4 lg:px-8">
+      <div className="mx-auto flex max-w-7xl items-center justify-between p-2 lg:px-8">
         <a href="#inicio" className="flex items-center gap-4">
-          <div className="relative h-12 w-12 shrink-0 sm:h-14 sm:w-14">
+          <div className="relative h-10 w-10 shrink-0 sm:h-14 sm:w-14">
             <Image
               src="/assets/logo.png"
               alt="Logo Andina Group"
@@ -23,8 +23,8 @@ export function Header() {
             />
           </div>
 
-          <div>
-            <p className="text-lg font-extrabold tracking-tight text-(--color-brand-green)">
+          <div className="hidden lg:flex">
+            <p className="lg:text-lg text-sm font-extrabold tracking-tight text-(--color-brand-green)">
               Andina group & Capital S.A.S
             </p>
             <p className="text-sm text-slate-500">
@@ -44,19 +44,21 @@ export function Header() {
             </a>
           ))}
 
-          <a  
+        </nav>
+        <div>
+          <a
             href="#contacto"
-            className="rounded-full bg-(--color-brand-orange) px-5 py-2.5 text-sm font-semibold text-white shadow-sm transition hover:bg-(--color-brand-orange-dark)"
+            className="rounded-full bg-(--color-brand-orange) p-2 text-sm lg:px-5 lg:py-2.5  font-semibold text-white shadow-sm transition hover:bg-(--color-brand-orange-dark) hidden flex:flex"
           >
             Cotizar ahora
           </a>
-          <a  
+          <a
             href="/login"
-            className="rounded-full bg-(--color-brand-orange) px-5 py-2.5 text-sm font-semibold text-white shadow-sm transition hover:bg-(--color-brand-orange-dark)"
+            className="rounded-full bg-(--color-brand-orange) p-2 lg:px-5 lg:py-2.5 text-sm font-semibold text-white shadow-sm transition hover:bg-(--color-brand-orange-dark)"
           >
             Iniciar sesión
           </a>
-        </nav>
+        </div>
       </div>
     </header>
   );
