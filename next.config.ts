32896f81bc7e767a64f3f8150a2a737057f4d8e4
Hugo Orielso <orielso.lozano15@gmail.com>
2026-04-05 19:45:36 -0500

    cambiando configuracion de next para setear cookies

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,17 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "standalone",
-
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: process.env.NODE_ENV === "production"
+          ? "http://srv-captain--backend:4000/:path*"  // ✅ red interna de CapRover
+          : "http://localhost:4000/:path*",             // ✅ desarrollo local
+      },
+    ];
+  },
 };
 
 export default nextConfig;
+
